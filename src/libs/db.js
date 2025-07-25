75db59ac287854f52d8f25f5b43a73b68b111ed6
Fernando Quintana <lferquint@gmail.com>
2025-07-24 21:37:02 -0600

    Lo voy a subir

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,13 +1,13 @@
 import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 dotenv.config({ path: '.env' })
-const PORT = 3306
+// const PORT = 3306
 
 const connection = await mysql.createConnection({
-  host: 'localhost',
-  port: PORT,
-  user: 'root',
-  database: 'vrintex2',
+  host: 'srv1135.hstgr.io',
+  // port: PORT,
+  user: 'u667942166_root',
+  database: 'u667942166_vrintex2',
   password: process.env.DB_PASSWORD
 })
 export default connection
